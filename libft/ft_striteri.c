12269190fa1f8674d3eb7c38eabdebfc0c1f82a0
duccio <duccio.ballini@gmail.com>
2022-10-24 19:42:40 +0200

    ft_striteri

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -10,17 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_striteri(char *s, void (*f)(unsigned int, char*))
-{
-	int		i;
+#include "libft.h"
 
-	i = 0;
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	unsigned int	i;
+
 	if (!s || !f)
+		return ;
+	i = 0;
+	while (s[i])
 	{
-		while (s[i])
-		{
-			f(i, s);
-			i++;
-		}
+		f(i, s + i);
+		i++;
 	}
 }
