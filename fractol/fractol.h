f52a7419f6a081fccf7648b9b42bb3c14b812e8a
duccio <duccio.ballini@gmail.com>
2023-02-13 17:38:17 +0100

    fractol

--- fractol/fractol.h
+++ fractol/fractol.h
@@ -6,23 +6,30 @@
 /*   By: dballini <dballini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 16:32:47 by dballini          #+#    #+#             */
-/*   Updated: 2023/02/10 17:19:16 by dballini         ###   ########.fr       */
+/*   Updated: 2023/02/13 17:28:03 by dballini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
+# include <math.h>
+# include <stdlib.h>
+# include <stdio.h>
+
 typedef struct	s_data {
 	void		*img;
 	char		*addr;
-	int		bits_per_pixel;
-	int		line_lenght;
-	int		endian;
+	int			bits_per_pixel;
+	int			line_lenght;
+	int			endian;
 }				t_data;
 
 typedef struct f_data
 {
+	void			*mlx;
+	void			*mlx_win;
+	t_data			img;
 	float	        MinRe;
 	float	        MaxRe;
 	float	        MinIm;
@@ -38,7 +45,10 @@ typedef struct f_data
     float           z_re;
     float           z_re2;
     float           z_im2;
-    int            isInside;
+    int           	isInside;
+	double			zoom;
+	double			movex;
+	double			movey;
 }       x_data;
 
 #endif
--- fractol/main.c
+++ fractol/main.c
@@ -6,7 +6,7 @@
 /*   By: dballini <dballini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 14:26:09 by dballini          #+#    #+#             */
-/*   Updated: 2023/02/10 17:37:21 by dballini         ###   ########.fr       */
+/*   Updated: 2023/02/13 17:37:53 by dballini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,38 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 
 }
 
-void	ft_mandelbrot()
+int		ft_mandelbrot(x_data *data, int n)
 {
+	while (n < data->MaxInteractions)
+	{
+		data->z_re2 = data->z_re * data->z_re;
+		data->z_im2 = data->z_im * data->z_im;
+		if (data->z_re2 + data->z_im2 > 4)
+		{
+			data->isInside = 0;
+			break ;
+		}
+		data->z_im = 2 * data->z_re * data->z_im + data->c_im;
+		data->z_re = data->z_re2 - data->z_im2 + data->c_re;
+		n++;
+	}
+	return (n);
+}
 
+unsigned int	ft_get_color(int n, float Zn)
+{
+	unsigned char	rgba[4];
+	unsigned int	color;
+	float	continuous_index;
+
+	if (Zn < 0)
+		Zn *= -1;
+	continuous_index = n + 1 - (log(2) / Zn) / log(2);
+	rgba[0] = (unsigned char)(sin(0.016 * continuous_index + 4) * 230 + 25);
+	rgba[1] = (unsigned char)(sin(0.013 * continuous_index + 2) * 230 + 25);
+	rgba[2] = (unsigned char)(sin(0.01 * continuous_index + 1) * 230 + 25);
+	color = (rgba[0] << 16) + (rgba[1] << 8) + (rgba[2] + 255);
+	return (color);
 }
 
 void	ft_calculate(x_data *data, t_data img)
@@ -33,35 +62,25 @@ void	ft_calculate(x_data *data, t_data img)
 	int		y;
 	int		n;
 
-	n = -1;
-	x = -1;
 	y = -1;
-	while (y++ <= data->WindowsHeight)
+	while (y++ < data->WindowsHeight)
 	{
+		x = -1;
 		data->c_im = data->MaxIm - y * data->Im_factor;
-		while (x++ <= data->WindowsWidth)
+		while (x++ < data->WindowsWidth)
 		{
 			data->c_re = data->MinRe + x * data->Re_factor;
 			data->z_re = data->c_re;
 			data->z_im = data->c_im;
 			data->isInside = 1;
-			while (n++ < data->MaxInteractions)
-			{
-				data->z_re2 = data->z_re * data->z_re;
-				data->z_im2 = data->z_im * data->z_im;
-				if (data->z_re2 + data->z_im2 > 4)
-				{
-					data->isInside = 0;
-					break ;
-				}
-				data->z_im = 2 * data->z_re * data->z_im + data->c_im;
-				data->z_re = data->z_re2 - data->z_im2 + data->c_re;
-			}
-			if (data->isInside == 1)
-				my_mlx_pixel_put(&img, x, y, 0xFFFFFF);
+			n = 0;
+			n = ft_mandelbrot(data, n);
+			if (data->isInside)
+				my_mlx_pixel_put(&img, x, y, 0x000000);
+			else
+				my_mlx_pixel_put(&img, x, y, ft_get_color(n, (data->z_re2 + data->z_im2)));
 		}
 	}
-
 }
 
 void	ft_set_values(x_data *data)
@@ -69,12 +88,12 @@ void	ft_set_values(x_data *data)
 	data->MinRe = -2;
 	data->MaxRe = 1.0;
 	data->MinIm = -1.2;
-	data->WindowsHeight = 600;
+	data->WindowsHeight = 500;
 	data->WindowsWidth = 600;
 	data->MaxIm = data->MinIm + (data->MaxRe - data->MinRe) * data->WindowsHeight / data->WindowsWidth;
-	data->Re_factor = (data->MaxRe - data->MinRe) / (data-> WindowsWidth - 1);
-	data->Im_factor = (data->MaxIm - data->MinIm) / (data-> WindowsHeight - 1);
-	data->MaxInteractions = 30; 
+	data->Re_factor = (data->MaxRe - data->MinRe) / (data->WindowsWidth - 1);
+	data->Im_factor = (data->MaxIm - data->MinIm) / (data->WindowsHeight - 1);
+	data->MaxInteractions = 30;
 }
 
 void	ft_start(x_data *data, t_data img)
@@ -82,25 +101,31 @@ void	ft_start(x_data *data, t_data img)
 	ft_calculate(data, img);
 }
 
+int	key_hook(int keyhook, x_data *data)
+{
+	if (!(data->mlx_win))
+		return (0);
+	if (keyhook == 65307)
+		exit (0);
+	if (keyhook == 116)
+		
+	mlx_put_image_to_window(data->mlx, data->mlx_win, data->img.img, 0, 0);
+	return (0);
+}
+
 int		main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
 	x_data	data;
-	int		i;
-	int		j;
 
-	j = 0;
-	i = 0;
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 600, 600, "Fractol");
-	img.img = mlx_new_image(mlx, 600, 600);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_lenght, &img.endian);
+	data.mlx = mlx_init();
+	data.mlx_win = mlx_new_window(data.mlx, 600, 500, "Fractol");
+	data.img.img = mlx_new_image(data.mlx, 600, 500);
+	data.img.addr = mlx_get_data_addr(data.img.img, &data.img.bits_per_pixel, &data.img.line_lenght, &data.img.endian);
 	
 	ft_set_values(&data);
-	ft_start(&data, img);
+	ft_start(&data, data.img);
 
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	mlx_put_image_to_window(data.mlx, data.mlx_win, data.img.img, 0, 0);
+	mlx_key_hook(data.mlx_win, key_hook, &data);
+	mlx_loop(data.mlx);
 }
